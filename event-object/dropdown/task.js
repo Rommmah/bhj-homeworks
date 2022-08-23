10f5cd20e832ca0a0fe28b49a9a561f21aa9b1b2
Rommmah <rommah@ro.ru>
2022-08-23 21:22:59 +0300

    ДЗ 3 - event-object (исправил)

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,19 +1,21 @@
 let valueBtn = document.querySelectorAll('.dropdown__value');
 
 for(let btn of valueBtn){
-	btn.onclick = () => {
-		let list = btn.nextElementSibling
-		if( list.classList.contains('dropdown__list') ){
-			list.classList.add('dropdown__list_active')
+	let list = btn.nextElementSibling
 
-			list.onclick = e => {
-				let text = e.target.closest('li')
-				if(text){
-					e.preventDefault()
-					btn.innerText = text.innerText
-					list.classList.remove('dropdown__list_active')
-				}
-			}
+	btn.onclick = () => {
+		if( list.classList.contains('dropdown__list') ){
+			list.classList.toggle('dropdown__list_active')
 		}
 	}
+
+	list.onclick = e => {
+		let text = e.target.closest('li')
+		if(text){
+			e.preventDefault()
+			btn.innerText = text.innerText
+			list.classList.remove('dropdown__list_active')
+		}
+	}
+
 }
